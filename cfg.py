4d0087103a92917b0f8b6ca3c4e56213c6a8551d
darkovvv <136109733+darkovvv@users.noreply.github.com>
2023-06-09 21:01:13 +0300

    Add files via upload

---
+++ cfg.py
@@ -0,0 +1,16 @@
+token = "vk1.a.cTR60I8M8X1l67zPg3vl_GpB2XhDFksGmx5Vyx2JqCRgIP22p793zpWSnC66Fr1NejbhqOdDD7FxCj5CztyoMfs1p7NC9brMz0ZHI70joomuEboLmf19E1Jq3lcltGD8sbitoC3jGPkxfnOlL0sIvHH8A9vvTdsZc53V3BfpbouazYZxG-6dde1851KXZPnk0tWrpM7AwL27iwCdnXrEbQ"
+# токен аккаунта
+chance = 1
+# шанс ответа
+acc_id = 11971510
+# айди пользователя
+user_ignore = []
+# игнор пользователей по айди
+chat_ignore = [29, 30, 32]
+# лист айди чатов с игнором
+send_delay = 1
+# время для отправки сообщений
+reply_only = 2
+# 1 - постоянно отвечать на сообщения 2 - отвечать по желанию 0 - не отвечать
+answer_only_on_replies = False
+# ответ только на реплаи
